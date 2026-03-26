5b79240c8d941ba760b82cba5899de8feb923627
Nsctc <nk7285399@gmail.com>
2026-03-26 13:48:47 +0530

    Change Client initialization with new credentials
    Updated Client initialization with new API credentials.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from handlers.play import play_handler
 from handlers.controls import controls
 from config import *
 
-app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client("bot",35356899 , api_hash=f1e9f66c392270e798b6e95413601ae9, bot_token=8697551682:AAGHZFUV7I6HC9NfPHZS1DWvm24kUJpNn8E)
 call = PyTgCalls(app)
 
 @app.on_message(filters.command("start"))
