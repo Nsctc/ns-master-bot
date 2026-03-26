de368409ecf159265adadb24364bbb6efeee74b6
Nsctc <nk7285399@gmail.com>
2026-03-26 12:25:50 +0530

    Add configuration variables for API and admin

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URL = os.getenv("MONGO_URL")
+
+ADMIN_USER = os.getenv("ADMIN_USER")
+ADMIN_PASS = os.getenv("ADMIN_PASS")
