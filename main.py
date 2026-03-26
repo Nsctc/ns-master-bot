3d87ac5d730b9171cc4a118170c4ab646e1ea497
Nsctc <nk7285399@gmail.com>
2026-03-26 17:14:09 +0530

    Change bot command handler and update client name

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ API_ID = int(os.environ["API_ID"])
 API_HASH = os.environ["API_HASH"]
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 
-app = Client("mybot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-@app.on_message(filters.command("start"))
-async def start(client, message):
-    print("Start command received")   # DEBUG
-    await message.reply_text("Bot working ✅")
+@app.on_message(filters.text)
+async def allmsg(client, message):
+    print("Message received:", message.text)
+    await message.reply_text("Reply aa gaya ✅")
 
 app.run()
